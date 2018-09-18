cefb4c6b0c0fb1e305130677c56448770a1ed2cb
Maddie Apparent Desktop <madeleine.westerstrom@apparent.com.au>
2018-09-18 17:44:43 +1000

    sign up feature

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -12,6 +12,11 @@ class SignUp extends Component {
 
     signUp(){
         console.log('this.state', this.state);
+        const { email, password } = this.state;
+        firebaseApp.auth().createUserWithEmailAndPassword(email, password)
+        .catch(error => {
+            console.log('error', error);
+        })
     }
 
     render() {
